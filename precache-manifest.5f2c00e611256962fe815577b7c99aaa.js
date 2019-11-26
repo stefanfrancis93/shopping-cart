bb8b05591097bf3ff6662cd96b1b9e58254823c9
Stefan Francis <stefan.mec.93@gmail.com>
2019-11-27 00:51:43 +0530

    Updates

---
+++ precache-manifest.5f2c00e611256962fe815577b7c99aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0061dd87b547595636097767d22f5d9a",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "3903b6121b7683b4bc11",
+    "url": "/shopping-cart/static/css/main.f9b8d071.chunk.css"
+  },
+  {
+    "revision": "94f12d8d77dfc2677e7b",
+    "url": "/shopping-cart/static/js/2.800ec665.chunk.js"
+  },
+  {
+    "revision": "3903b6121b7683b4bc11",
+    "url": "/shopping-cart/static/js/main.842360cc.chunk.js"
+  },
+  {
+    "revision": "bc3bc61530b19116fa7e",
+    "url": "/shopping-cart/static/js/runtime-main.e1594773.js"
+  },
+  {
+    "revision": "5fe0e55f8e19bc4cc3201876dce7b224",
+    "url": "/shopping-cart/static/media/spinner.5fe0e55f.gif"
+  }
+]);
